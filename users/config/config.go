006e92cd8be55c1e754e15ca0ef31cb226c826ef
Meghana <meghana@example.com>
2025-02-27 12:43:59 +0530

    Updated users and config files

--- users/config/config.go
+++ users/config/config.go
@@ -3,10 +3,10 @@ package config
 type Properties struct {
 	Port           string `env:"USERS_SRV_PORT" env-default:"51000"`
 	Host           string `env:"USERS_HOST" env-default:"localhost"`
-	DBUser         string `env:"DB_USER" env-default:""`
-	DBPass         string `env:"DB_PASS" env-default:""`
-	DBName         string `env:"DB_NAME" env-default:""`
-	DBURL          string `env:"DB_URL" env-default:""`
+	DBUser         string `env:"DB_USER" env-default:"velamaumamaheswari"`
+	DBPass         string `env:"DB_PASS" env-default:"umavelama"`
+	DBName         string `env:"DB_NAME" env-default:"cluster-1"`
+	DBURL          string `env:"DB_URL" env-default:"mongodb+srv://%s:%s@cluster-1.khrok.mongodb.net/?retryWrites=true&w=majority"`
 	UserCollection string `env:"USER_COLLECTION" env-default:"users"`
-	JwtTokenSecret string `env:"JWT_TOKEN_SECRET" env-default:""`
+	JwtTokenSecret string `env:"JWT_TOKEN_SECRET" env-default:"CheeseCherryDanishCake"`
 }
